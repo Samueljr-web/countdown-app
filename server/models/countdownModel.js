8a3a69e310fbe2e4c222d1dd766c2989fd64e21d
Abdulkabir sultan <abdulkabirsultan@gmail.com>
2022-11-25 00:55:38 +0100

    create model for countdown

---
+++ server/models/countdownModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const countDownSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please provide a title to your countdown'],
+      unique: true,
+    },
+    date: {
+      type: String,
+      required: [true, 'please provide a countdown date'],
+    },
+    hash: {
+      type: String,
+      required: [true, 'please provide hash'],
+    },
+    time: String,
+    reason: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('CountDown', countDownSchema);
