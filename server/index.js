6fedb1d3bc7ba4aef7660c62749d494a3760c391
Temitope Agbaje <agbajeinioluwa@gmail.com>
2022-11-24 22:23:16 +0100

    Homepage form api added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const db = require("./database/db")
-require("dotenv").config()
+const db = require("./database/db");
+const HomepageRouter = require("./routes/homepageRoutes");
+require("dotenv").config();
 
 const app = express();
-db.connectToMongoDB()
+db.connectToMongoDB();
 
 const PORT = process.env.PORT || 3700;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("<h2>Lets Countdown!</h2>");
+});
+
+app.use("/", HomepageRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
