17d589d90cf230f3d8ebcd52644d08dcd737f2cd
Abdulkabir sultan <88564842+abdulkabirsultan@users.noreply.github.com>
2022-11-25 18:24:39 +0100

    Add hash functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CounterPage from "./pages/CounterPage";
-
+import {useEffect} from 'react';
 function App() {
 
   // use API call to get user inputted time and date.
   const userDate = (new Date("2022-11-30"));
-
+ useEffect (()=>{
+  const hash = Math.random().toString(36).substring(7);
+  const hasher = localStorage.getItem("hash")
+  if(!hasher){
+      localStorage.setItem("hash", hash);
+}
+return;
+},[]);
   return (
     <>
       <Router>
