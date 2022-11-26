698194e8593aeb48484632cfd59b57e37b21ace0
Idris Samir <samzidris11@gmail.com>
2022-11-26 22:04:33 +0100

    feat: configure react-router-dom path-string

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,6 @@ import HomePage from "./pages/HomePage";
 import CounterPage from "./pages/CounterPage";
 import "react-toastify/dist/ReactToastify.css";
 
-//test
-import axios from 'axios';
-// import { getURL } from './constants/index.js'
-//end
-
 function App() {
   useEffect(() => {
     const hash = Math.random().toString(36).substring(7);
@@ -21,29 +16,17 @@ function App() {
   }, []);
   const [respData, setRespData] = useState({})
 
-  //test
-  // var ID = '663825e8ee0934fd3624c3b0';
-  // var title = 'eventer';
-  //end
-
-  var ID = `${respData._id}`;
-  var title = `${respData.title}`;
+  let ID = `${respData._id}`;
+  let title = `${respData.title}`;
   console.log(ID);
   console.log(title)
-
-  //test
-  axios.get("https://lets-countdown-production.up.railway.app/api/v1/663825e8ee0934fd3624c3b0/eventer")
-  .then((response) => {
-    console.log(response.data);
-  })
-  //end
 
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={ <HomePage respData={respData} setRespData={setRespData} dataID={ID} dataTitle={title} />} />
-          <Route path={`/${ID}/${title}`} element={<CounterPage dataID={ID} dataTitle={title}/>}/>
+          <Route path="/:id/:title" element={<CounterPage dataID={ID} dataTitle={title}/>}/>
         </Routes>
       </Router>
     </>
