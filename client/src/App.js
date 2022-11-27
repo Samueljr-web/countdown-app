79c8c3084b8caedb439d4a8debbaf9feb82773e5
Abdulkabir sultan <abdulkabirsultan@gmail.com>
2022-11-27 12:58:30 +0100

    add params for unique link

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,46 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CounterPage from "./pages/CounterPage";
-import "react-toastify/dist/ReactToastify.css";
-
+import { useEffect, useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CounterPage from './pages/CounterPage';
+import 'react-toastify/dist/ReactToastify.css';
+import NotFound from './pages/NotFound';
 function App() {
   useEffect(() => {
     const hash = Math.random().toString(36).substring(7);
-    const hasher = localStorage.getItem("hash");
+    const hasher = localStorage.getItem('hash');
     if (!hasher) {
-      localStorage.setItem("hash", hash);
+      localStorage.setItem('hash', hash);
     }
     return;
   }, []);
-  const [respData, setRespData] = useState({})
+  const [respData, setRespData] = useState({});
 
   let ID = `${respData._id}`;
   let title = `${respData.title}`;
   console.log(ID);
-  console.log(title)
+  console.log(title);
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={ <HomePage respData={respData} setRespData={setRespData} dataID={ID} dataTitle={title} />} />
-          <Route path="/:id/:title" element={<CounterPage dataID={ID} dataTitle={title}/>}/>
+          <Route
+            path='/'
+            element={
+              <HomePage
+                respData={respData}
+                setRespData={setRespData}
+                dataID={ID}
+                dataTitle={title}
+              />
+            }
+          />
+          <Route
+            path='/:id/:eventTitle'
+            element={<CounterPage dataID={ID} dataTitle={title} />}
+          />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>
     </>
